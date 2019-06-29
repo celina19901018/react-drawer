10ac697414f52886fedb584e40cc3e71cbecfbd4
celina520524 <celina520524@163.com>
2019-06-29 23:38:14 +0800

    feat: 增加login页面

--- src/containers/App/Index.js
+++ src/containers/App/Index.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from '../Login';
 import logo from './logo.svg';
 import './App.css';
+
+function AppRouter() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Route path="/" exact component={App}></Route>
+      <Route path="/login" component={Login}></Route>
+    </Router>
+  )
+}
 
 function App() {
   return (
@@ -23,4 +45,4 @@ function App() {
   );
 }
 
-export default App;
+export default AppRouter;
---
+++ src/containers/Login/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './style.less';
+
+class Login extends React.Component {
+  render() {
+    return(
+      <form>
+        <label htmlFor="username">
+          <input
+          id="username"
+          placeholder="请输入用户姓名"
+          type="text" />
+        </label>
+        <label htmlFor="password">
+          <input
+          id="password"
+          placeholder="请输入登陆密码"
+          type="password" />
+        </label>
+        <button>登陆</button>
+      </form>
+    )
+  }
+}
+
+export default Login
